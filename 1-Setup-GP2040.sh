0dbdfa2a2c9ed07d00e6993cf766db1b7bde10dd
MegaBitmap <93689649+MegaBitmap@users.noreply.github.com>
2025-12-19 09:13:56 -0800

    fix dependencies and update board list

--- 1-Setup-GP2040.sh
+++ 1-Setup-GP2040.sh
@@ -2,20 +2,29 @@
 
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install git curl cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential python3-venv
+sudo apt -y install git curl cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib build-essential python3-venv
 
 # Install pico sdk
-cd $HOME
+cd "$HOME"
 git clone https://github.com/raspberrypi/pico-sdk.git
 cd pico-sdk
-git checkout tags/1.5.1
+git checkout tags/2.1.1-correct-picotool
 git submodule update --init
 cd ..
 
 # Install node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-. $HOME/.nvm/nvm.sh
-nvm install v20
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+# Download and install Node.js:
+nvm install 20
+# Verify the Node.js version:
+node -v # Should print "v20.19.6".
+# Verify npm version:
+npm -v # Should print "10.8.2".
 
 # Download GP2040 source code
-git clone https://github.com/OpenStickCommunity/GP2040-CE
+git clone https://github.com/OpenStickCommunity/GP2040-CE.git
+cd GP2040-CE
+git submodule update --init
--- 2-Build-GP2040.sh
+++ 2-Build-GP2040.sh
@@ -4,19 +4,38 @@
 # The following line must be changed depending on which pico board is used:
 export GP2040_BOARDCONFIG=Pico
 # Supported Boards (choose one):
-# ARCController, Pico, BentoBox, FightboardV3, FightboardV3Mirrored, 
-# FlatboxRev4, FlatboxRev5, FlatboxRev5RGB, FlatboxRev5Southpaw, Granola, 
-# KB2040, KeyboardConverter, Haute42COSMOX, Liatris, Mavercade, OpenCore0, 
-# OpenCore0WASD, PicoAnn, PicoFightingBoard, RanaTadpole, ReflexCtrlGenesis6, 
-# ReflexCtrlNES, ReflexCtrlSaturn, ReflexCtrlSNES, ReflexCtrlVB, ReflexEncodeV1.2, 
-# ReflexEncodeV2.0, RP2040AdvancedBreakoutBoard, RP2040AdvancedBreakoutBoardUSBPassthrough, 
-# RP2040MiniBreakoutBoard, SparkFunProMicro, WaveshareZero, Stress, SGFDevices
+# ARCController              OpenCore0MIXUP
+# ASiLVLABC                  OpenCore0WASD
+# ASiLVLWASD                 PXPGamepad
+# BentoBox                   Pico
+# Blank                      Pico2
+# FlatboxRev4                PicoAnn
+# FlatboxRev5                PicoFightingBoard
+# FlatboxRev5RGB             PicoW
+# FlatboxRev5Southpaw        RP2040AdvancedBreakoutBoard
+# FlatboxRev5USBPassthrough  RP2040AdvancedBreakoutBoardUSBPassthrough
+# FlatboxRev8                RP2040MiniBreakoutBoard
+# Granola                    RP2040MiniBreakoutBoardUSBPassthrough
+# GranolaBeacon              ReflexCtrlGenesis6
+# Haute42COSMOX              ReflexCtrlNES
+# Haute42COSMOXMLite         ReflexCtrlSNES
+# Haute42COSMOXMUltra        ReflexCtrlSaturn
+# Haute42COSMOXXAnalog       ReflexCtrlVB
+# KB2040                     ReflexEncodeV1.2
+# KeyboardConverter          ReflexEncodeV2.0
+# Liatris                    SGFDevices
+# MavercadeRev1              SeeedXIAORP2040
+# MavercadeRev2              SparkFunProMicro
+# MavercadeRev3              SparkFunProMicroRP2350
+# MiSTercadeV2               Stress
+# OSUMGP-RP2040              WaveshareZero
+# OpenCore0                  ZeroRhythm
 
-export PICO_SDK_PATH=$HOME/pico-sdk/
-. $HOME/.nvm/nvm.sh
+export PICO_SDK_PATH="$HOME/pico-sdk/"
+\. "$HOME/.nvm/nvm.sh"
 
 # clean install
-cd $HOME/GP2040-CE/www/
+cd "$HOME/GP2040-CE/www/"
 npm ci
 
 # Build WWW
